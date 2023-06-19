6491b5ea7fe4d7b1cea85dcc039d9972204aefe6
wsw954 <32828432+wsw954@users.noreply.github.com>
2023-06-19 18:45:34 -0400

    Commit 21: Recode the configurations file for cloning & mutation consistency, add action handlers for sponsor & remover

--- configurations/honda/civic.js
+++ configurations/honda/civic.js
@@ -17,43 +17,42 @@ export function handleChange(currentState, selectedOption) {
     const {
       ancestor = null,
       sponsor = null,
+      remover = null,
       optionPackage = null,
       parent = null,
       rival = null,
       sibling = null,
     } = action;
     if (ancestor != null) {
-      updatedState = handleAncestor(ancestor, currentState, selectedOption);
+      updatedState = handleAncestor(updatedState, selectedOption);
     }
     if (sponsor != null) {
-      updatedState = handleSponsor(sponsor, currentState, selectedOption);
+      updatedState = handleSponsor(updatedState, selectedOption);
+    }
+    if (remover != null) {
+      updatedState = handleRemover({ ...currentState }, selectedOption);
     }
 
     if (optionPackage != null) {
-      updatedState = handleOptionPackage(currentState, selectedOption);
+      updatedState = handleOptionPackage(updatedState, selectedOption);
     }
     if (parent != null) {
-      updatedState = handleParent(currentState, selectedOption);
+      updatedState = handleParent(updatedState, selectedOption);
     }
     if (rival != null) {
-      updatedState = handleRival(currentState, selectedOption);
+      updatedState = handleRival(updatedState, selectedOption);
     }
     if (sibling != null) {
-      updatedState = handleSibling(currentState, selectedOption);
+      updatedState = handleSibling(updatedState, selectedOption);
     }
   }
   return handleRegularOptionChange(updatedState, selectedOption);
 }
 
-function handleAncestor(ancestorArray, state, selectedOption) {
+function handleAncestor(state, selectedOption) {
   const { categoryName, type, action, serial, checked } = selectedOption;
 
-  const newChoicesAvailable = getDescendents(
-    ancestorArray,
-    categoryName,
-    serial
-  );
-
+  const newChoicesAvailable = getDescendents(categoryName, serial);
   const ancestorCategoryChoices = state.availableChoices.filter(
     (choice) => choice.categoryName === categoryName
   );
@@ -74,13 +73,46 @@ function handleAncestor(ancestorArray, state, selectedOption) {
   };
 }
 
-function handleSponsor(sponsorArray, state, selectedOption) {
-  const { categoryName, type, action, serial, checked } = selectedOption;
-  let newState = { ...state };
-  return newState;
+function handleSponsor(state, selectedOption) {
+  const { categoryName, action: { sponsor } = {}, serial } = selectedOption;
+  let recipientChoices = getRecipients(categoryName, serial);
+
+  let updatedAvailableChoices = recipientChoices.reduce(
+    (choices, choice) => addAvailableChoices(state, choice),
+    [...state.availableChoices]
+  );
+  return { ...state, availableChoices: updatedAvailableChoices };
 }
 
-function handleOptionPackage(updatedState, selectedOption) {
+function handleRemover(state, selectedOption) {
+  const { action: { remover } = {} } = selectedOption;
+
+  const updatedAvailableChoices = state.availableChoices
+    .map((category) => ({
+      ...category,
+      choices: category.choices.filter(
+        (choice) => !remover.includes(choice.serial)
+      ),
+    }))
+    .filter((category) => category.choices.length > 0);
+
+  const updatedSelectedChoices = state.selectedChoices
+    .map((category) => ({
+      ...category,
+      choices: category.choices.filter(
+        (choice) => !remover.includes(choice.serial)
+      ),
+    }))
+    .filter((category) => category.choices.length > 0);
+
+  return {
+    ...state,
+    availableChoices: updatedAvailableChoices,
+    selectedChoices: updatedSelectedChoices,
+  };
+}
+
+function handleOptionPackage(state, selectedOption) {
   let newState = { ...state, optionPackageProcessed: true };
   return newState;
 }
@@ -101,20 +133,15 @@ function handleSibling(state, selectedOption) {
 function handleRegularOptionChange(state, selectedOption) {
   const { categoryName, type, action, serial, checked } = selectedOption;
   if (checked) {
-    return addOption(state, selectedOption);
+    return addSelectedChoices(state, selectedOption);
   } else {
-    return removeOption(state, selectedOption);
+    return removeSelectedChoices(state, selectedOption);
   }
 }
 
-function getDescendents(ancestorArray, categoryName, serial) {
+//Helper function for handleAncestor()
+function getDescendents(categoryName, serial) {
   let filteredData = data;
-
-  if (ancestorArray.length > 0) {
-    filteredData = data.filter((item) =>
-      ancestorArray.includes(item.categoryName)
-    );
-  }
   const newChoicesAvailable = filteredData
     .filter((item) => item.categoryName !== categoryName) // Skip the object with same categoryName
     .map((item) => ({
@@ -122,11 +149,43 @@ function getDescendents(ancestorArray, categoryName, serial) {
       choices: item.choices.filter(
         (choice) => choice.descendent && choice.descendent.includes(serial)
       ),
-    }));
+    }))
+    .filter((item) => item.choices.length > 0); // Exclude objects where 'choices' array is empty
+
   return newChoicesAvailable;
 }
 
-function addOption(state, selectedOption) {
+//Helper function for handleSponsor()
+function getRecipients(categoryName, serial) {
+  const additionalChoicesAvailable = data
+    .filter((item) => item.categoryName !== categoryName) // Skip the object with same categoryName
+    .map((item) => ({
+      ...item,
+      choices: item.choices.filter(
+        (choice) => choice.recipient && choice.recipient.includes(serial)
+      ),
+    }))
+    .filter((item) => item.choices.length > 0); // Exclude objects where 'choices' array is empty
+
+  return additionalChoicesAvailable;
+}
+
+// function getRemovals(categoryName, serial) {
+//   let filteredData = data;
+//   const removalChoices = filteredData
+//     .filter((item) => item.categoryName !== categoryName) // Skip the object with same categoryName
+//     .map((item) => ({
+//       ...item,
+//       choices: item.choices.filter(
+//         (choice) => choice.recipient && choice.recipient.includes(serial)
+//       ),
+//     }))
+//     .filter((item) => item.choices.length > 0); // Exclude objects where 'choices' array is empty
+
+//   return removalChoices;
+// }
+
+function addSelectedChoices(state, selectedOption) {
   const { categoryName, type, serial } = selectedOption;
 
   const category = data.find((item) => item.categoryName === categoryName);
@@ -175,30 +234,59 @@ function addOption(state, selectedOption) {
   return { ...state, selectedChoices: updatedSelectedChoices };
 }
 
-function removeOption(state, selectedOption) {
+function removeSelectedChoices(state, selectedOption) {
   const { categoryName, serial } = selectedOption;
 
-  let updatedSelectedChoices = [...state.selectedChoices]; // Make a copy of selectedChoices
+  const updatedSelectedChoices = state.selectedChoices
+    .map((category) => {
+      if (category.categoryName !== categoryName) {
+        return category;
+      }
 
-  const categoryIndex = updatedSelectedChoices.findIndex(
-    (item) => item.categoryName === categoryName
+      const newChoices = category.choices.filter(
+        (choice) => choice.serial !== serial
+      );
+
+      if (newChoices.length === 0) {
+        return null;
+      }
+
+      return { ...category, choices: newChoices };
+    })
+    .filter((category) => category !== null);
+
+  return { ...state, selectedChoices: updatedSelectedChoices };
+}
+
+function addAvailableChoices(state, availableOption) {
+  const { categoryName, component, choices } = availableOption;
+
+  let updatedAvailableChoices = [...state.availableChoices];
+  let categoryIndex = updatedAvailableChoices.findIndex(
+    (choice) => choice.categoryName === categoryName
   );
 
   if (categoryIndex !== -1) {
-    const choiceIndex = updatedSelectedChoices[categoryIndex].choices.findIndex(
-      (item) => item.serial === serial
-    );
-
-    if (choiceIndex !== -1) {
-      // Remove the choice
-      updatedSelectedChoices[categoryIndex].choices.splice(choiceIndex, 1);
-
-      // If no choices remain in this category, remove the category
-      if (updatedSelectedChoices[categoryIndex].choices.length === 0) {
-        updatedSelectedChoices.splice(categoryIndex, 1);
-      }
+    if (
+      !updatedAvailableChoices[categoryIndex].choices.some(
+        (choice) => choice.serial === choices[0].serial
+      )
+    ) {
+      updatedAvailableChoices[categoryIndex] = {
+        ...updatedAvailableChoices[categoryIndex],
+        choices: [
+          ...updatedAvailableChoices[categoryIndex].choices,
+          choices[0],
+        ],
+      };
     }
+  } else {
+    updatedAvailableChoices.push({
+      categoryName: categoryName,
+      component: component,
+      choices: [choices[0]],
+    });
   }
 
-  return { ...state, selectedChoices: updatedSelectedChoices };
+  return updatedAvailableChoices;
 }
